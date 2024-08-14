cea349127123ab9d1d88c40f368821157a41ecdc
lixumin@xizhi-ai.com <lixumin@xizhi-ai.com>
2024-08-14 11:33:47 +0800

    Initial Commit

---
+++ demo Watch App/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo Watch App
+//
+//  Created by lixumin on 2024/8/14.
+//
+
+import SwiftUI
+
+@main
+struct demo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by lixumin on 2024/8/14.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
